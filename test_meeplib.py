a7cb0bd6e9c6d8952d2f0d4b26d346df625de76c
Chelsea Carr <carrche2@msu.edu>
2012-01-27 00:36:58 -0500

    Added the unit test for project3

--- test_meeplib.py
+++ test_meeplib.py
@@ -10,28 +10,37 @@ import meeplib
 class TestMeepLib(unittest.TestCase):
     def setUp(self):
         u = meeplib.User('foo', 'bar')
-        m = meeplib.Message('the title', 'the content', u)
+        m = meeplib.Message( 'the content', u)
+        t = meeplib.Thread('TestingPage')
+        t.add_post(m)
 
     def test_for_message_existence(self):
-        x = meeplib.get_all_messages()
-        assert len(x) == 1
-        assert x[0].title == 'the title'
-        assert x[0].post == 'the content'
+        x = meeplib.get_all_threads()
+        y = x[0].get_all_posts()
+        assert len(y) == 1
+        assert x[0].title == 'TestingPage'
+        assert y[0].post == 'the content'
 
     def test_message_ownership(self):
         x = meeplib.get_all_users()
         assert len(x) == 1
         u = x[0]
 
-        x = meeplib.get_all_messages()
-        assert len(x) == 1
-        m = x[0]
+        threads = meeplib.get_all_threads()
+        for thread in threads:
+            messages = thread.get_all_posts()
+            for message in messages:
+                print message.author
+                assert message.author == u
 
-        assert m.author == u
+    def test_get_next_user(self):
+        x = meeplib._get_next_user_id()
+        assert x != None
 
     def tearDown(self):
-        m = meeplib.get_all_messages()[0]
-        meeplib.delete_message(m)
+        m = meeplib.get_all_threads()
+        p = m[0].get_all_posts('id')
+        m[0].delete_post(p[0])
 
         u = meeplib.get_all_users()[0]
         meeplib.delete_user(u)
